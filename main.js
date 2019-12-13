a42097874ef061eb79fdaedaf42826fb8f3c2358
sukhadiatej99 <57688669+sukhadiatej99@users.noreply.github.com>
2019-12-13 00:34:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-var temperature = new five.Thermometer({
+var Readable = require("stream").Readable;  
+var util = require("util");  
+util.inherits(MyStream, Readable);  
+function MyStream(opt) {  
+  Readable.call(this, opt);
+}
+MyStream.prototype._read = function() {};  
+// hook in our stream
+process.__defineGetter__("stdin", function() {  
+  if (process.__stdin) return process.__stdin;
+  process.__stdin = new MyStream();
+  return process.__stdin;
+});
+
+var five = require("johnny-five");
+var board = new five.Board({
+	repl: false
+});
+
+var valueDiv1 = document.querySelector("#moisture");
+var valueDiv2 = document.querySelector("#photo");
+var valueDiv3 = document.querySelector("#temp");
+// var ctx = document.getElementById('myChart').getContext('2d');
+
+board.on("ready", function(){
+	var temperature = new five.Thermometer({
 		controller: "LM35",
 		pin: "A2",
 		freq: 5000
@@ -92,3 +117,48 @@ var photoresistor = new five.Sensor({
   			photoAlert.innerHTML = "Too much Light";
   		}
 	});
+	var moistureSensor = new five.Sensor({
+		pin: "A1",
+		freq: 5000,
+		threshold: 2
+	});
+
+	moistureSensor.on("change", function(){
+		// var moisture = this.value;
+		// valueDiv1.innerHTML = moisture;
+		let ctx = document.getElementById('myChart3').getContext('2d');
+		let labels = ['Moisture'];
+		let colorHex = ['#A3DC63'];
+		var per = 100 * (681 - this.value) / 681;
+		moist.innerHTML = Math.round(per) + "%";
+		let myChart = new Chart(ctx, 
+		{
+  			type: 'doughnut',
+  			data: 
+  			{
+    			datasets: [{
+      				data: [680-this.value,this.value],
+      				backgroundColor: colorHex
+    			}],
+    			labels: labels
+  			},
+  			options: 
+  			{
+    			responsive: true,
+  			}	
+
+		})
+		if(per <= 25)
+  		{
+  			moistAlert.innerHTML = "Too Dry";
+  		}
+  		else if(per > 25 && per <= 75)
+  		{
+  			moistAlert.innerHTML = "Perfect Moisture";
+  		}
+  		else
+  		{
+  			moistAlert.innerHTML = "Too Light";
+  		}
+	});
+});
